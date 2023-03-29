f008f3b152cb08a271bdea2d20fb848858907cb1
{hiromi30} <{yarufou@gmail.com}>
2023-03-29 21:50:07 +0900

    更新

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
+import { useGetUserID } from "../hooks/useGetUserID";
 
 export const Home = () => {
-  const [ recipes, setRecipes] = useState([]);
-
+  const [recipes, setRecipes] = useState([]);
+  const userID = useGetUserID();
+  
   useEffect(() => {
     const fetchRecipe = async () => {
         try {
-          const response = await axios.get("http://localhost:3001/recipes")
+          const response = await axios.get("http://localhost:3001/recipes");
           setRecipes(response.data)
         } catch (err) {
           console.error(err)
@@ -16,12 +18,26 @@ export const Home = () => {
 
     fetchRecipe()
   }, []);
+
+  const saveRecipe = async(recipeID) => {
+      try {
+        const response = await axios.put("http://localhost:3001/recipes",{
+          recipeID,
+          userID,
+        });
+        console.log(response)
+      } catch (err) {
+        console.error(err)
+      }
+  }
+
   return (
   <div>
     <h1> Recipes </h1>
     <ul>
       {recipes.map((recipe) => (
         <li key={recipe._id}>
+          <button onClick={() => saveRecipe(recipe.id)}> Save </button>
           <div>
             <h2>{recipe.name}</h2>
           </div>
