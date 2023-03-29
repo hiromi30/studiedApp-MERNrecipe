9bb887901389fd3e3f6a8edded15a8305e3fac4e
{hiromi30} <{yarufou@gmail.com}>
2023-03-29 16:54:01 +0900

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config"
+
+
+import {userRouter} from "./routes/users.js"
+import {recipeRouter} from "./routes/recipes.js"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter)
+app.use("/recipes", recipeRouter)
+
+
+mongoose.connect(process.env.MONGODB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("server started"));
